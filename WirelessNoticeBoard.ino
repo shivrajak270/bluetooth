0e228d0acb6e70ea42c2a5e569404aae831bfe0a
shivrajak270 <147083124+shivrajak270@users.noreply.github.com>
2024-06-13 00:00:12 +0530

    Add files via upload

---
+++ WirelessNoticeBoard.ino
@@ -0,0 +1,80 @@
+#include <SoftwareSerial.h> #include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F,20,3); SoftwareSerial mySerial (0,1); //(RX, TX);
+
+String val = "No Data";
+
+String oldval;
+
+String newval = "No Data";
+
+int i = 0;
+
+void setup() {
+// put your setup code here, to run once:
+
+lcd.init();
+
+// initialize the Icd
+
+lcd.init();
+
+// Print a message to the LCD,
+
+lcd.backlight();
+
+// put your setup
+
+Icd.begin(16,2);
+
+mySerial.begin(9600);
+
+Serial.begin(9600);
+
+lcd.setCursor(0, 0);
+
+lcd.print("Wireless Notice");
+
+lcd.setCursor(0, 1);
+
+lcd.print(" Board ");
+
+delay(3000);
+
+lcd.clear();
+
+lcd.print("Welcome!");
+
+}
+
+void loop() {
+
+val = mySerial.readString();
+
+val.trim();
+
+Serial.println(val);
+
+if(val != oldval) {
+
+newval = val;
+
+}
+
+lcd.clear();
+
+lcd.setCursor(i, 0);
+
+lcd.print(newval);
+
+i++;
+
+if(i >= 15) {
+
+i = 0;
+
+}
+
+val = oldval;
+
+}
